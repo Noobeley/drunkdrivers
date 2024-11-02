4d2f6d6c7446aa8d60ef837abf2ee986af72bdcd
borcya <bor.jankaevikt@gmail.com>
2024-11-02 23:23:42 +0100

    Ivo trying his best

--- Assets/Scripts/RaycastBoxColliderWithLoadAnimation.cs
+++ Assets/Scripts/RaycastBoxColliderWithLoadAnimation.cs
@@ -1,73 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class RaycastBoxColliderWithLoadAnimation : MonoBehaviour
 {
-    [SerializeField] private XRController controller; // Assign the controller in the inspector
+    public float detectionRange = 10f;
     [SerializeField] private GameObject loadAnimation; // Reference to the loading animation GameObject
-    [SerializeField] private float rayDistance = 10f;
-    [SerializeField] private LayerMask layerMask; // Assign layer mask to target specific layers if needed
+    [SerializeField] private InputActionReference[] interactAction; // Input actions for triggering the animation
+    [SerializeField] private LayerMask layerMask; // Layer mask to target specific layers if needed
 
     private bool isAnimationPlaying = false;
+    private BoxCollider currentBoxCollider;
 
-    private void Update()
+    void Update()
     {
+        DetectBoxCollider();
+
+        // Check if either input action is triggered
+        if ((interactAction[0].action.triggered || interactAction[1].action.triggered) && currentBoxCollider != null)
+        {
+            ToggleLoadAnimation();
+        }
+    }
+
+    void DetectBoxCollider()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        // Determine the ray origin
-        Vector3 rayOrigin;
-        Vector3 rayDirection;
-
-        if (controller != null && controller.inputDevice.isValid)
-        {
-            rayOrigin = controller.transform.position;
-            rayDirection = controller.transform.forward;
-        }
-        else
-        {
-            rayOrigin = Camera.main.transform.position;
-            rayDirection = Camera.main.transform.forward;
-        }
-
         // Perform raycast
-        if (Physics.Raycast(rayOrigin, rayDirection, out hit, rayDistance, layerMask))
+        if (Physics.Raycast(ray, out hit, detectionRange, layerMask))
         {
+            // Check if the collider is a BoxCollider
             if (hit.collider is BoxCollider)
             {
-                // Show loading animation if not already playing
-                if (!isAnimationPlaying)
-                {
-                    loadAnimation.SetActive(true);
-                    isAnimationPlaying = true;
-                    Debug.Log("Playing Load Animation on: " + hit.collider.gameObject.name);
-                }
+                currentBoxCollider = hit.collider as BoxCollider;
             }
             else
             {
-                StopLoadAnimation();
+                currentBoxCollider = null;
             }
         }
         else
         {
-            StopLoadAnimation();
+            currentBoxCollider = null;
         }
-
-        // Optional: Visualize the ray in the scene view
-        Debug.DrawRay(rayOrigin, rayDirection * rayDistance, Color.red);
     }
 
-    // Stops the load animation
-    private void StopLoadAnimation()
+    private void ToggleLoadAnimation()
     {
-        if (isAnimationPlaying)
+        if (!isAnimationPlaying)
+        {
+            loadAnimation.SetActive(true);
+            isAnimationPlaying = true;
+            Debug.Log("Playing Load Animation on: " + currentBoxCollider.gameObject.name);
+        }
+        else
         {
             loadAnimation.SetActive(false);
             isAnimationPlaying = false;
             Debug.Log("Stopped Load Animation");
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * detectionRange);
+    }
 }
-
-
